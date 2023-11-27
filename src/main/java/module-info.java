1d2eba95488d46b62feb6c0a5f13694864914dae
AlKl1M <al.klimkovetskiy@gmail.com>
2023-11-27 17:28:14 +0200

    module-info.java configured and hello-view.fxml deleted.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,7 @@
 module com.bfu.javafxchatapp {
     requires javafx.controls;
     requires javafx.fxml;
-            
-                            
-    opens com.bfu.javafxchatapp to javafx.fxml;
-    exports com.bfu.javafxchatapp;
+
+    exports com.bfu.javafxchatapp.server;
+    opens com.bfu.javafxchatapp.server to javafx.fxml;
 }
